de2e1c8ff1bd983908cfda8c85b46fd0d2faf87a
Giwa Mubarak <giwaomogiwa@gmail.com>
2024-06-09 21:15:20 +0100

    changes

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+const input = document.getElementById('input');
+const output = document.getElementById('output');
+const keys = document.getElementById('.key');
+
+
+const mubarak = function (){
+    console.log('yes!')
+}
+mubarak();
